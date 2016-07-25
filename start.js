afac2a361c1528f7fa2142c738983e77c64c2040
gaomiaofei <gaomiaofei@shanqb.com>
2016-07-25 16:11:28 +0800

    master

---
+++ start.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gao on 16/7/25.
+ */
+var server = require("./lib/novae");
+var router = require("./lib/router");
+var config = require("./config");
+
+server.start(router.route,config.config.port);
